9c57a543e0ae1d15d310459c18d48b84f9da569b
Odair Lampe <odairlampe123@gmail.com>
2022-03-26 01:25:47 -0300

    Testing fixed background

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-$("#sidecall").on('click', function(){
-    $('.ui.sidebar')
+$("#sidecall").on('click', () => {
+  $('.ui.sidebar')
     .sidebar('toggle');
-  });
+});
+
+$("#sidecall").on('click', () => {
+  setTimeout(() => {
+    $(".line1").removeClass('hamburger-line')
+    $(".line1").addClass('hamburger-x-1')
+    $(".line2").removeClass('hamburger-line')
+    $(".line2").addClass('hamburger-x-2')
+    $(".line3").removeClass('hamburger-line')
+
+    hamburgerLines();
+  }, 100);
+})
+
+function hamburgerLines() {
+  if ($("#sidebar").hasClass('visible')) {
+    setTimeout(() => {
+      hamburgerLines();
+    }, 300);
+  }
+  else {
+    $(".line1").removeClass('hamburger-x-1')
+    $(".line2").removeClass('hamburger-x-2')
+
+    $(".line1, .line2, .line3").addClass('hamburger-line')
+  }
+}
